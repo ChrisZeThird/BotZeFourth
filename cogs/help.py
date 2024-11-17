ff2984d3eee47d1f87b492f1debca2489f3ecfbe
Chris Ze Third <wilantchri.game@gmail.com>
2024-11-17 10:32:05 +0100

    Log system when a command rises, sent to the support server

--- cogs/help.py
+++ cogs/help.py
@@ -23,8 +23,13 @@ class Help(commands.Cog):
             # Check if the argument is a category or a command
             command = self.bot.get_command(arg)
             if command:
-                # Display information about the specified command
-                embed = discord.Embed(title=f"`{command.name}`", description=command.help, color=0xff0000)
+                # Display information about the specified command wit args
+                embed = discord.Embed(
+                    title=f"`{command.name}`",
+                    description=command.help or "No description provided.",
+                    color=0xff0000
+                )
+                embed.add_field(name="Usage", value=f"/{command.name} `{command.signature}`", inline=False)
                 await ctx.send(embed=embed)
             else:
                 # Check if the argument is a category
